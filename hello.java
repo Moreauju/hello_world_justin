e5981f29051ed85856fcdaf90f32aac39b157ecb
Moreauju <justin0607easily@gmail.com>
2024-10-30 16:13:17 -0400

    commit

--- hello.java
+++ hello.java
@@ -1,14 +1,14 @@
-// a simple program to practice common Github commands 
+// a simple program to practice common Github commands
 
 public class hello {
 
     public static void main(String [] args) {
 
        // initialize variables
-       String firstName = "Justin";
+        String firstName = "Justin";
 
        // delete this line
-       System.out.println("Hello, my name is " + firstName);
+        System.out.println("Hello, my name is " + firstName);
 	
        // Using the value of the variable above, output a message in the format, "Hello, I am Alex".
 
